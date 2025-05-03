e64f089f1726d28a7b1780afb0e0b83db50d8ae3
gaurav0973 <codejourneypanda@gmail.com>
2025-05-03 23:02:42 +0530

    Problem added

---
+++ 02_machineCoding/01_questionPractice.js
@@ -0,0 +1,29 @@
+// Problem: Create an object representing a type of tea with properties for name, type and caffeine content
+
+
+// Problem: Access and print the name and type properties of the tea object
+
+
+// Problem: Add a new property origin to the tea object
+
+
+// Problem: Change the caffeine level of the tea object to "Medium"
+
+
+// Problem: Remove the type property from the tea object
+
+
+// Problem: Check if the tea object has a property origin
+
+
+// Problem: Use a for...in loop to print all properties of the tea object
+
+
+// Problem: Create a nested object representing different types of teas and their properties
+
+// Problem: Create a copy of the tea object
+
+
+// Problem: Add a custom method describe to the tea object that returns a description string
+
+// Problem: Merge two objects representing different teas into one
